c70f6ea70ec4aca8c2a7ea23e37d1e9a29b142cd
Claudio <95890307+claudio-plaza@users.noreply.github.com>
2022-08-11 02:08:39 -0300

    examen python ejercicio 10

---
+++ valoracio-final-python.py
@@ -0,0 +1,8 @@
+#10) Crear una lista denominada “Clientes” con los nombres de los diferentes  dueños de perros.
+#          Juan,  Mario,  Ariel,  Josefina,  Marianella.
+#Ordenarla alfabéticamente y mostrarla por pantalla.
+#El archivo debe ser subido a la rama de la persona que creó el código, ya que los docentes verificaremos la participación por el
+#historia
+clientes = ["Juan", "Mario",  "Ariel",  "Josefina",  "Marianella"]
+orden_alf = sorted(clientes)
+print(* orden_alf)
