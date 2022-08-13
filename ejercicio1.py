3266fe07e77d70d6e0841400c93f04bfc3d26fd3
Laurix86 <grossoanalaura@gmail.com>
2022-08-13 12:45:21 -0300

    Ejercicio Python - inciso 8

---
+++ ejercicio1.py
@@ -0,0 +1,6 @@
+## Inciso 8 Crear una tupla en Python con el nombre de "Historial4"
+# la cual contenga los siguientes valores: 7510, 7960, 76180, 800, 410
+
+Historial4 = (7510, 7960, 76180, 800, 4100)
+
+print("El valor mínimo de atención gastada en Olivia fue ", min(Historial4))
