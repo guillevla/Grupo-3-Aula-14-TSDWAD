1e1cbd679b956544788fd5f37f5dfc498661bb35
Guillermo Ariel Peveri <91349395+guillevla@users.noreply.github.com>
2022-08-14 01:32:56 -0300

    Create Ejercicio2.py

---
+++ Ejercicio2.py
@@ -0,0 +1,11 @@
+#Introducción a la programación:
+
+#2)   Crear una lista en Python denominada “Dueno2”  y recorrerla con un bucle  mostrando sus elementos por pantalla con excepción del DNI y el apellido. Los elementos de la lista son:
+# 23546987,  “Maria”,  “Perez”, 4789689,  “Pueyrredon  811”
+#que representan los datos del dueño de un perro (DNI, nombre, apellido, teléfono y dirección) 
+
+Dueno2 = [23546987,  "Maria",  "Perez", 4789689,  "Pueyrredon  811"]
+for x in Dueno2:
+    if x != Dueno2[0]:
+        if x != Dueno2[2]:
+            print(x)
