63662cc6e592373c7c49bf132291c76b2ef8e122
Laurix86 <grossoanalaura@gmail.com>
2022-08-14 12:01:13 -0300

    función python

--- ejercicio1.py
+++ ejercicio1.py
@@ -2,5 +2,14 @@
 # la cual contenga los siguientes valores: 7510, 7960, 76180, 800, 410
 
 Historial4 = (7510, 7960, 76180, 800, 4100)
+valor_minimo = Historial4[0]
 
+for precio in Historial4:
+    if precio < valor_minimo:
+        valor_minimo = precio
+
+
+print("El valor mínimo de atención gastada en Olivia fue ", valor_minimo)
+
+## Alternativa simple
 print("El valor mínimo de atención gastada en Olivia fue ", min(Historial4))
